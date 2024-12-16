f6e7d0e115e999a19eb0f1f9a8cec7bd3a096a0a
Voya <voya@voyacode.com>
2024-12-16 18:17:50 +0200

    Change A* to be more generic

--- src/day16.ts
+++ src/day16.ts
@@ -1,7 +1,6 @@
 // https://adventofcode.com/2024/day/16
 
-import { GraphNode } from './algorithms/graph-node.ts';
-import { MinHeap } from './data-structures/heap.ts';
+import { AStarNode, findTargetsWithAStar, findTargetWithAStar } from './algorithms/a-star.ts';
 import { Coordinate, Direction, parseGrid } from './utils.ts';
 
 const EAST = { xDir: 1, yDir: 0 } as const;
@@ -11,15 +10,33 @@ const START = 'S';
 const END = 'E';
 type NodeType = typeof WALL | typeof START | typeof END;
 
+interface PathOptions {
+  map: NodeType[][];
+  end: Coordinate;
+}
+
 export function part1(input: string) {
   const { map, startCoord, endCoord } = parseInput(input);
-  return findBestPaths(map, startCoord, endCoord)[0].score;
+  const start = new Path(startCoord, EAST, 0);
+  return findTargetWithAStar(start, (node) => node.coordinate.x === endCoord.x && node.coordinate.y === endCoord.y, {
+    map,
+    end: endCoord,
+  }).score;
 }
 
 export function part2(input: string) {
   const { map, startCoord, endCoord } = parseInput(input);
+  const start = new Path(startCoord, EAST, 0);
+  const bestPaths = findTargetsWithAStar(
+    start,
+    (node) => node.coordinate.x === endCoord.x && node.coordinate.y === endCoord.y,
+    {
+      map,
+      end: endCoord,
+    },
+    { findAllBestPaths: true },
+  );
   const uniqueCoords = new Set<string>();
-  const bestPaths = findBestPaths(map, startCoord, endCoord, { findAllBestPaths: true });
   for (const path of bestPaths) {
     uniqueCoords.add(`${path.coordinate.x},${path.coordinate.y}`);
     for (const subpath of path.getPath()) {
@@ -52,56 +69,7 @@ function parseInput(input: string) {
   };
 }
 
-/**
- * Find quickest path by using A*
- * Distinct states are identified by location and direction
- */
-function findBestPaths(
-  grid: NodeType[][],
-  start: Coordinate,
-  goal: Coordinate,
-  options: { findAllBestPaths?: boolean } = {},
-) {
-  const findAllBestPaths = options.findAllBestPaths ?? false;
-  const pathHeap = new MinHeap<Path>((path) => path.getMinScore(goal));
-  // stateKey => minScore
-  const visitedStates = new Map<string, number>();
-
-  const startPath = new Path(start, EAST, 0);
-  pathHeap.addItem(startPath);
-
-  const bestPaths: Path[] = [];
-
-  while (pathHeap.length) {
-    const nextQuickestPath = pathHeap.pop();
-    const stateKey = nextQuickestPath.getStateKey();
-    if (
-      visitedStates.has(stateKey) &&
-      (!findAllBestPaths || nextQuickestPath.getMinScore(goal) > visitedStates.get(stateKey)!)
-    ) {
-      continue;
-    }
-    if (
-      nextQuickestPath.coordinate.x === goal.x &&
-      nextQuickestPath.coordinate.y === goal.y
-    ) {
-      if (bestPaths.length && findAllBestPaths && bestPaths[0].score !== nextQuickestPath.score) {
-        return bestPaths;
-      }
-      bestPaths.push(nextQuickestPath);
-      if (!findAllBestPaths) {
-        return bestPaths;
-      }
-    }
-    visitedStates.set(stateKey, nextQuickestPath.getMinScore(goal));
-    const adjacentPaths = nextQuickestPath
-      .getAdjacentPaths(grid);
-    pathHeap.addItems(adjacentPaths);
-  }
-  throw new Error('Path not found');
-}
-
-class Path extends GraphNode<Path> {
+class Path extends AStarNode<Path, PathOptions> {
   constructor(
     public coordinate: Coordinate,
     private direction: Direction,
@@ -112,10 +80,10 @@ class Path extends GraphNode<Path> {
     this.nodeState.previousNode = previous;
   }
 
-  getAdjacentPaths(grid: NodeType[][]) {
+  getAdjacentNodes({ map }: PathOptions) {
     const adjacentPaths: Path[] = [];
     const nextCoord = getNextCoordinate(this.coordinate, this.direction);
-    if (grid[nextCoord.y][nextCoord.x] !== WALL) {
+    if (map[nextCoord.y][nextCoord.x] !== WALL) {
       adjacentPaths.push(new Path(nextCoord, this.direction, this.score + 1, this));
     }
     const leftDirection = {
@@ -128,10 +96,10 @@ class Path extends GraphNode<Path> {
     } as Direction;
     const leftCoord = getNextCoordinate(this.coordinate, leftDirection);
     const rightCoord = getNextCoordinate(this.coordinate, rightDirection);
-    if (grid[leftCoord.y][leftCoord.x] !== WALL) {
+    if (map[leftCoord.y][leftCoord.x] !== WALL) {
       adjacentPaths.push(new Path(leftCoord, leftDirection, this.score + 1001, this));
     }
-    if (grid[rightCoord.y][rightCoord.x] !== WALL) {
+    if (map[rightCoord.y][rightCoord.x] !== WALL) {
       adjacentPaths.push(new Path(rightCoord, rightDirection, this.score + 1001, this));
     }
     // Note: Assuming 180 degree turn is never part of best path
@@ -142,7 +110,7 @@ class Path extends GraphNode<Path> {
     return `${this.coordinate.x},${this.coordinate.y}:${this.direction.xDir},${this.direction.yDir}`;
   }
 
-  getMinScore(end: Coordinate) {
+  getMinScore({ end }: PathOptions) {
     const xDiff = Math.abs(end.x - this.coordinate.x);
     const yDiff = Math.abs(end.y - this.coordinate.y);
     const turnScore = xDiff !== 0 && yDiff !== 0 ? 1000 : 0;
